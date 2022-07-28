40647748445fe35233b3691034b5f467c153cd67
dinesh <dineshnagulapally@gmail.com>
2022-07-28 17:29:07 -0500

    initial project. closes 1

---
+++ BeerServiceMicroService/src/main/java/com/example/beerservicemicroservice/BeerServiceMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.beerservicemicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerServiceMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerServiceMicroServiceApplication.class, args);
+	}
+
+}
